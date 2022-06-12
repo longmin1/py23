470c270d2245fe3005bccb64abd03f0f2a96dd69
longmin1 <601940651@qq.com>
2022-06-12 15:49:46 +0800

    app自动化  添加成员，删除成员，外出打卡 PO简单封装

--- app_appium_shizhan_of_mine_第一节/pageobject/editmemberpage.py
+++ app_appium_shizhan_of_mine_第一节/pageobject/editmemberpage.py
@@ -3,10 +3,12 @@ from selenium.webdriver.common.by import By
 from app_appium_shizhan_of_mine_第一节.pageobject.basepage import BasePage
 
 class EditMemberPage(BasePage):
+    __DEL_MEMBER=(By.XPATH, '//*[@text="删除成员"]')
+    __DEL_BUTTON=(By.ID,'com.tencent.wework:id/c0w')
     def del_member(self):
-        del_member=(By.XPATH, '//*[@text="删除成员"]')
-        self.scroll_wait_ele(del_member).click()
-        self.driver.find_element(By.ID,'com.tencent.wework:id/c0w').click()
+
+        self.scroll_wait_ele(self.__DEL_MEMBER).click()
+        self.find(self.__DEL_BUTTON).click()
         from app_appium_shizhan_of_mine_第一节.pageobject.address import AddressPage
         return AddressPage(self.driver)
 
--- app_appium_shizhan_of_mine_第一节/pageobject/personalinformationpage.py
+++ app_appium_shizhan_of_mine_第一节/pageobject/personalinformationpage.py
@@ -3,13 +3,15 @@ from selenium.webdriver.common.by import By
 from app_appium_shizhan_of_mine_第一节.pageobject.basepage import BasePage
 
 class PersonalInformationPage(BasePage):
+    __MENU_LOCATION=(By.ID,'com.tencent.wework:id/izt')
+    __EDIT_MEMBER_LOCATION=(By.ID,'com.tencent.wework:id/bn7')
 
     def click_menu(self):
         '''点击个人信息的右上角三点'''
-        self.driver.find_element(By.ID,'com.tencent.wework:id/izt').click()
+        self.find(self.__MENU_LOCATION).click()
         return self
 
     def click_edit_member(self):
-        self.driver.find_element(By.ID,'com.tencent.wework:id/bn7').click()
+        self.find(self.__EDIT_MEMBER_LOCATION).click()
         from app_appium_shizhan_of_mine_第一节.pageobject.editmemberpage import EditMemberPage
         return EditMemberPage(self.driver)
